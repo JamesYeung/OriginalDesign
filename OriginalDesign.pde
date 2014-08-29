e5ec5bf36c05919ef44fef0cf99c6ad330dde57e
JamesYeung <Jamesyeung19@yahoo.com>
2014-08-29 09:56:51 -0700

    Added OriginalDesign

--- OriginalDesign.pde
+++ OriginalDesign.pde
@@ -1,9 +1,21 @@
+int x = 250;
+int y = 250;
+int speed = 5;
 void setup()
 {
-
+	size(500,500);
+	frameRate(40);
 }
 void draw()
 {
-  ellipse(50,50,50,50);
+	x=x+speed;
+	y=y+speed;
+	background(0);
+  	ellipse(x,y,50,50);
+  	if(x==500 && y==500)
+  	{
+  		x=0;
+  		y=0;
+  		fill((int)(Math.random()*256),(int)(Math.random()*256),(int)(Math.random()*256));
+  	}
 }
-
--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -14,15 +14,27 @@ import java.io.IOException;
 
 public class OriginalDesign extends PApplet {
 
+int x = 250;
+int y = 250;
+int speed = 5;
 public void setup()
 {
-
+	size(500,500);
+	frameRate(40);
 }
 public void draw()
 {
-  ellipse(50,50,50,50);
+	x=x+speed;
+	y=y+speed;
+	background(0);
+  	ellipse(x,y,50,50);
+  	if(x==500 && y==500)
+  	{
+  		x=0;
+  		y=0;
+  		fill((int)(Math.random()*256),(int)(Math.random()*256),(int)(Math.random()*256));
+  	}
 }
-
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "OriginalDesign" };
     if (passedArgs != null) {
